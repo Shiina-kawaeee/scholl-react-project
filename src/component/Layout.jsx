d2c8951d77e7d1c892464ff03a1a7999fe14f0da
Monai <monai@gmail.com>
2023-12-13 13:43:09 +0700

    ใส่ Router

---
+++ src/component/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+import '../css/Layout.css';
+
+function Layout() {
+  return (
+    <>
+        <header className='narbar'>
+            <div className='logo'>
+            <h1><a href=''>logo</a></h1>
+            </div>
+            <ul className='left'>
+            <li><a href='#'>home</a></li>
+            <li><a href='#'>shop</a></li>
+            <li><a href='#'>about</a></li>
+            </ul>
+            <ol>
+            <li><a href='#'>&#128682; login</a></li>
+            <li><a href='#'>about</a></li>
+            </ol>
+        </header>
+        <Outlet />
+    </>
+  )
+}
+
+export default Layout
