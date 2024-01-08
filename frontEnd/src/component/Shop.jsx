8b1eaf2067af6e12bd4b65e1196cea44d3bf6a04
monai@scholl <monai@scholl>
2024-01-08 11:00:42 +0700

    add frontEnd and backEnd

---
+++ frontEnd/src/component/Shop.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../css/shop.css';
+
+function Shop() {
+  return (
+    <div className='grid-contrainer'>
+      <div className='grid-item'>
+          <img src="" alt="" />
+      </div>
+      <div className='grid-item'>
+        <img src="" alt="" />
+      </div>
+      <div className='grid-item'>
+        <img src="" alt="" />
+      </div>
+      <div className='grid-item'>
+        <img src="" alt="" />
+      </div>
+      
+    </div>
+  )
+}
+
+export default Shop
