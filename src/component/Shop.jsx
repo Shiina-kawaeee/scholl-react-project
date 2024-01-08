0002ca5d5b640120ab264ec60b0a6c160500b370
pradook <pradook123456789@hotmail.com>
2024-01-08 10:46:55 +0700

    ใส่รูปมาให้

--- src/component/Shop.jsx
+++ src/component/Shop.jsx
@@ -4,7 +4,9 @@ import '../css/shop.css'
 function Shop() {
   return (
     <div className='grid-contrainer'>
-      <div className='grid-item'>1</div>
+      <div className='grid-item'>
+        <img src="https://www.mitrpholsugar.com/wp-content/uploads/2022/01/%E0%B8%84%E0%B8%A3%E0%B8%B1%E0%B8%A7%E0%B8%8B%E0%B8%AD%E0%B8%87-1568x1044-1.jpg" alt="" />
+      </div>
       <div className='grid-item'>1</div>
       <div className='grid-item'>1</div>
       <div className='grid-item'>1</div>
