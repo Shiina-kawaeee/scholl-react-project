ce4d5492fe524e4b9229f6692c9c77562b711de4
Monai <monainai2546@gmail.com>
2024-01-08 23:34:37 +0700

    add new php function

--- backEnd/condb.php
+++ backEnd/condb.php
@@ -1,15 +1,19 @@
 <?php 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'nompang';
+    function connectToDatabase(){
+        $servername = 'localhost';
+        $username = 'root';
+        $password = 'root';
+        $dbname = 'nompang';
 
-    try{
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        try{
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo 'Connection succesfully';
-    }catch (PDOException $e) {
-        echo 'Connection failed: '. $e->getMessage();
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            return array('status' => 'success' ,'DbConStatus' => "Database Connect Success fully", 'connection' => $conn);
+
+        }catch (PDOException $e) {
+            return array('status' => 'failed' ,'DbConStatus' => "Database Connect FAILED : " .$e->getMessage());
+        }
     }
 ?>
--- backEnd/getData.php
+++ backEnd/getData.php
@@ -1,25 +1,50 @@
 <?php 
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: access");
-    header("Access-Control-Allow-Methods: POST");
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        header("Access-Control-Allow-Origin: http://localhost:5173");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+        header("Content-Type: application/json; charset=UTF-8");
+        http_response_code(200);
+        exit;
+    }
+
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Headers: Content-Type,
-        Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     include './condb.php';
 
-    $data = json_decode(file_get_contents('php://input'));
+    $connectionInfo = connectToDatabase();
 
-    $collectionName = $data->collectionName;
-    $sql = "SELECT * FROM $collectionName";
+    if($connectionInfo['status'] === 'success'){
+        $conn = $connectionInfo['connection'];
 
-    $result = $conn->query($sql);
+        $data = json_decode(file_get_contents('php://input'));
+        $collectionName = $data->collectionName;
+        $sql = "SELECT * FROM $collectionName";
 
-    if($result){
-        $response['data'] = array('status' => 'valid');
-        echo json_encode($response);
+        try{
+            $result = $conn->query($sql);
+
+            if($result){
+                $row = $result->fetchAll(PDO::FETCH_ASSOC);
+
+                $response['status'] = array('queryStatus' => 'resultValid');
+                $response['data'] = array('result' => $row);
+                echo json_encode($response);
+            } else {
+                $response['data'] = array('queryStatus' => 'resultInvalid');
+                echo json_encode($response);
+            }
+        }catch(PDOException $e) {
+                header("HTTP/1.1 500 Internal Server Error");
+
+                $response['status'] = array('queryStatus' => 'invalid');
+                $response['error'] = array('error' => $e->getMessage());
+                echo json_encode($response);
+        }
     } else {
-        $response['data'] = array('status' => 'invalid');
-        echo json_encode($response);
+        echo json_encode(array('error' => $connectionInfo['DbConStatus']));
     }
 ?>
